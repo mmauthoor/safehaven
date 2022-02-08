f634df1be42415f4312290215d0d9a6a21b0c537
Hughsin23 <hughsy_23@hotmail.com>
2022-02-08 15:16:01 +1100

    added base form time/date

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import SimpleMap, { MapContainer } from './Map.js';
+import ReportForm from './components/ReportForm'
+
 
 function App() {
   return (
     <div className="App">
       <h1>Safe Haven</h1>
       <SimpleMap />
+      <ReportForm />
     </div>
   );
 }
