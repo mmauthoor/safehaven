7cdc8f0f7128f9e23f32229ca3cc7d244b146f43
hannah <hannahcaitlinc@outlook.com>
2022-02-08 15:12:09 +1100

    map added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import SimpleMap, { MapContainer } from './Map.js';
 
 function App() {
   return (
     <div className="App">
       <h1>Safe Haven</h1>
+      <SimpleMap />
     </div>
   );
 }
