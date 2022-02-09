e41b94f1fa0caaba666a6287669cdbb8e3f12fc5
hannah <hannahcaitlinc@outlook.com>
2022-02-09 13:02:14 +1100

    intital app styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
+import logo from './greylogo-02.png'
+import searchIcon from './search.png'
 import './App.css';
 import SimpleMap, { MapContainer } from './Map.js';
 import ReportForm from './components/ReportForm'
 
+
 function App() {
   return (
     <div className="App">
-      <h1>Safe Haven</h1>
+      <ul>
+        <li>
+        <li>
+          <img className="search-icon" src={searchIcon} alt="Logo" />
+        </li>
+          <button className="report-btn">
+            <a href="default.asp">Report Incident</a>
+          </button>
+        </li>
+      </ul>
+      <div>
+        <img className="logo" src={logo}></img>
+      </div>
+
+      <div className="about-section">
+      <h1>What is safehaven?</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sun in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+
+
+      <div className="location-info-box">
+        
+      </div>
+      <div className="map-container">
       <SimpleMap />
+      </div>
+
+      <div className="report-form-container">
       <ReportForm />
+      </div>
     </div>
   );
 }
