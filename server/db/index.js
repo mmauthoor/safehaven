e00fcd7369600aa1ef1335e662154706d404f0c6
Carter Deacon <carter.deacon@gmail.com>
2022-02-09 12:25:11 +1000

    relocated server.js

--- server/db/index.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require('pg')
-
-const pool = new Pool({ // this will need to change in deployment, replaced by a heroku string
-  database: 'safehaven'
-})
-
-module.exports = pool
-
