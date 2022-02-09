f1b36a15f2c52a71dc06081b59826eba81bd8748
hannah <hannahcaitlinc@outlook.com>
2022-02-09 18:17:16 +1100

    added map bounds
    resolved conflicts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './greylogo-02.png'
+import logo from './logo.png'
 import searchIcon from './search.png'
 import './App.css';
 import ShowMap, { MapContainer } from './Map.js';
@@ -26,7 +26,6 @@ function App() {
       <h1>What is safehaven?</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sun in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
-
 
       <div className="location-info-box">
         
