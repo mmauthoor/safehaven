054e35cfc6b8a9bde40d7f7cc1e000d340f3e1b0
anguscg <angus.cg@hotmail.com>
2022-02-09 13:01:06 +1100

    added manual markers before refactor

--- src/Map.js
+++ src/Map.js
@@ -1,19 +1,35 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
 import { Link } from "react-router-dom";
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
-class SimpleMap extends React.Component {
-  static defaultProps = {
-    center: {
-      lat: -37.8136,
-      lng: 144.9631
-    },
-    zoom: 11
-  };
 
-  render() {
+
+
+function SimpleMap() {
+  
+  const Marker = ({  }) => (
+    <div style={{
+      color: 'white', 
+      background: 'grey',
+      padding: '15px 10px',
+      display: 'inline-flex',
+      textAlign: 'center',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: '100%',
+      transform: 'translate(-50%, -50%)'
+    }}>
+    </div>
+  );
+  
+  const [marker, setMarker] = useState({})
+
+  useEffect(() => {
+    fetch('/api/reports').then(res => console.log(res.json())).then(res => setMarker(res))
+  },[])
+
     return (
       // Important! Always set the container height explicitly
       <div style={{ height: "100vh", width: "100%" }}>
@@ -21,13 +37,15 @@ class SimpleMap extends React.Component {
           bootstrapURLKeys={{
             key: process.env.REACT_APP_GOOGLE_MAPS_API
           }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
+          defaultCenter={{lat: -37.8136, lng: 144.9631}}
+          defaultZoom={11}
+          
         >
+        <Marker lat={-37.8136} lng={144.9631} />
         </GoogleMapReact>
       </div>
     );
   }
-}
+
 
 export default SimpleMap;
