d71e4f3a598d2034d71abe2187113ea22b9acc1d
anguscg <angus.cg@hotmail.com>
2022-02-10 11:18:12 +1100

    added file Map.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import searchIcon from './search.png'
 import './App.css';
 import ShowMap, { MapContainer } from './Map.js';
 import ReportForm from './components/ReportForm'
+import {Routes, Route, Link} from 'react-router-dom'
+
+
+function Home() {
+  return (
+    <div className="about-section">
+      <h1>What is safehaven?</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sun in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+  )
+}
 
 
 function App() {
@@ -14,29 +25,18 @@ function App() {
           <img className="search-icon" src={searchIcon} alt="Logo" />
         </li>
           <button className="report-btn">
-            <a href="default.asp">Report Incident</a>
+            <a href="/reports/new">Report Incident</a>
           </button>
         </li>
       </ul>
       <div>
         <img className="logo" src={logo}></img>
       </div>
-
-      <div className="about-section">
-      <h1>What is safehaven?</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sun in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
-
-      <div className="location-info-box">
-        
-      </div>
-      <div className="map-container">
-      <ShowMap />
-      </div>
-
-      <div className="report-form-container">
-      <ReportForm />
-      </div>
+      
+      <Routes> 
+        <Route path='/' element={<> <Home /> <ShowMap /> </>} />
+        <Route path="/reports/new" element={<> <ReportForm /> <ShowMap /> </>}/>
+      </Routes>
     </div>
   );
 }
