c4f97a1fb012df81b09f883003434d1b81c6d297
Carter Deacon <carter.deacon@gmail.com>
2022-02-10 10:01:37 +1000

    complete mapbounds function and server communication

--- src/Map.js
+++ src/Map.js
@@ -4,16 +4,6 @@ import './Map.css'
 import { Link } from "react-router-dom";
 import { useState, useEffect } from 'react'
 import MarkerImage from './MarkerImage.png'
-import { application_name } from "pg/lib/defaults";
-
-// function getVisibleMarkers(coords){
-  // setMarkers([])
-//   let sql = `SELECT * FROM reports 
-//   WHERE (latitude BETWEEN ${mapBounds.botLat} AND ${mapBounds.topLat}) 
-//   AND (longitude BETWEEN ${mapBounds.botLng} AND ${mapBounds.topLng});`
-
-//   return db.query(sql) //check format of db
-// }
 
 function ShowMap() {
   
@@ -22,24 +12,18 @@ function ShowMap() {
   );
   
   const [markers, setMarkers] = useState([]);
-  // const [bounds, setBounds] = useState();
-
-  useEffect(() => {
-    fetch('http://localhost:8000/api/reports')
-    .then(res => res.json())
-    .then(res => setMarkers(res))
-  },[])
-
-  // app.get('/api/reports', (req, res) => {
-  //   const coords = {
-  //     botlat: req.query.botlat,
-  //     toplat: req.query.toplat,
-  //     botlng: req.query.botlng,
-  //     toplng: req.qeuery.toplng
-  //   }
-  //   const result = getVisibleMarkers(coords)
-  //   result.then(dbres => res.json(dbres.rows))
-  // }
+  
+  const handleBoundsChange = (mapNew) => {
+    let corners = {
+      topLat: mapNew.marginBounds.ne.lat,
+      topLng: mapNew.marginBounds.ne.lng,
+      botLat: mapNew.marginBounds.sw.lat,
+      botLng: mapNew.marginBounds.sw.lng,
+    }
+    fetch(`http://localhost:8000/api/reports?topLat=${corners.topLat}&topLng=${corners.topLng}&botLat=${corners.botLat}&botLng=${corners.botLng}`)
+      .then(res => res.json())
+      .then(res => setMarkers(res))
+    }
   
     return (
       // Important! Always set the container height explicitly
@@ -50,9 +34,9 @@ function ShowMap() {
           }}
           defaultCenter={{lat: -37.8136, lng: 144.9631}}
           defaultZoom={12}
-          onChange={e => console.log(e)}
+          onChange={e => handleBoundsChange(e)}
         >
-        {markers.map(marker => <Marker lat={marker.lat} lng={marker.lng}/>)}
+        {markers.map((marker, index) => <Marker key={index} lat={marker.lat} lng={marker.lng}/>)}
         </GoogleMapReact>
       </div>
     );
