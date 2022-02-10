fa9ae27c3b55890841bcbea1a78e4a2cfcda6df0
hannah <hannahcaitlinc@outlook.com>
2022-02-10 17:48:28 +1100

    readme file updated

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function Home() {
   return (
     <div className="about-section">
       <h1>What is safehaven?</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sun in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>safehaven is an app designed for women and other minority groups as a means to help us feel safer outside of the comfort of our own homes. safehaven allows you to search and log incidents of sexual assault across Australia anonymously. We understand how scary it can be to report sexual assault. None of your information will be shared publicly. One day we hope this app will no longer be necessary, but until then please feel free to share your experiences so that others are aware of potentially dangerous areas.</p>
     </div>
   )
 }
