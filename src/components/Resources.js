63e97a9cee1d71c73f4fcbc11fb65e990da87be0
Hughsin23 <hughsy_23@hotmail.com>
2022-02-10 14:00:05 +1100

    added resources

---
+++ src/components/Resources.js
@@ -0,0 +1,27 @@
+import reachlogo from "../reachlogo.png"
+import hotlinelogo from "../hotlinelogo.png"
+
+export default function Resources () {
+
+
+    return (
+        <div className="resources-container">
+            <section className="help-article">
+                <h3>Unsure if you've been assaulted?</h3>
+                <a href="https://au.reachout.com/articles/sexual-assault#2"><img src={reachlogo} alt="" /></a>
+            </section>
+            <section className="help-article">
+                <h3>For additional support, reach out here</h3>
+                <a href="https://www.sacl.com.au/"><img src={hotlinelogo} alt="" /></a>
+            </section>
+
+
+
+
+
+
+        </div>
+
+
+    )
+}
